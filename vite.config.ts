7b0e7c220d98514265688499aeed6577d7970953
Iramsaba-24 <iramsaba_k.championxi@outlook.com>
2025-12-30 17:32:59 +0530

    Components added

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,23 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import path from 'path';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
-// https://vite.dev/config/
+
 export default defineConfig({
-  plugins: [react()],
-})
+  base: '/', 
+  plugins: [react(), tsconfigPaths()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    }
+  },
+  define: {
+    __APP_ENV__: JSON.stringify('production'),
+  },
+  server: {
+    port: 3005,
+    open: true,
+    host: true
+  }
+});
