9c1992979735bf0809daa6cd3f5cda53eb365e61
Iramsaba-24 <iramsaba_k.championxi@outlook.com>
2026-01-31 11:00:02 +0530

    update

--- src/renderer/src/containers/layout/Header.tsx
+++ src/renderer/src/containers/layout/Header.tsx
@@ -64,7 +64,7 @@ const menuItems = [
   { text: 'Dashboard', icon: <DashboardIcon />, path: '/dashboard' },
   { text: 'Customers', icon: <PeopleIcon />, path: URL_PATH.Customer },
   { text: 'Doctors', icon: <LocalHospitalIcon />, path: URL_PATH.Doctors },
-  { text: 'Distributors', icon: <LocalShippingIcon />, path: URL_PATH.DistributorsTable   },
+  { text: 'Distributors', icon: <LocalShippingIcon />, path: URL_PATH.DistributorsPage},
   { text: 'Inventory', icon: <Inventory2Icon />, path: URL_PATH.Inventory },
 
   {
